d2fb2106040e6815bada5a508d780bec2239c552
FabioCostaa <fabio.glz4@gmail.com>
2023-05-24 16:14:58 -0300

    first commit

---
+++ src/data/questions_complete.js
@@ -0,0 +1,119 @@
+const data = [
+  {
+    category: "Grega",
+    questions: [
+      {
+        question: "Qual deus é considerado o senhor dos mares ?",
+        options: ["Zeus", "Hades", "Poseidon", "Apolo"],
+        answer: "Poseidon",
+        tip: "Possui um tridente",
+      },
+      {
+        question: "Quem é o deus do sol na mitologia grega ?",
+        options: ["Hermes", "Dionísio", "Hefesto", "Apolo"],
+        answer: "Apolo",
+        tip: "Também é associado a música, poesia e profecia.",
+      },
+      {
+        question: "Qual deusa é conhecida como a deusa do amor e da beleza ?",
+        options: ["Atena", "Afrodite", "Hera", "Artemis"],
+        answer: "Afrodite",
+        tip: "Ela também é tratada na Mitologia Romana com o nome de deusa de Vênus.",
+      },
+      {
+        question: "Qual herói mitológico é conhecido por seu calcanhar vulnerável ?",
+        options: ["Perseu", "Aquiles", "Ulisses", "Jasão"],
+        answer: "Aquiles",
+        tip: "Famoso guerreiro da mitologia grega, tinha apenas um ponto fraco",
+      },
+      {
+        question: "Qual é o nome da deusa grega da sabedoria ?",
+        options: ["Atena", "Afrodite", "Deméter", "Hera"],
+        answer: "Atena",
+        tip: "Capital da Grécia possui o seu nome",
+      },
+    ],
+  },
+  {
+    category: "Nórdica",
+    questions: [
+      {
+        question: "Quem é o deus da trapaça e da travessura na mitologia nórdica ?",
+        options: ["Odin", "Loki", "Freyja", "Baldur"],
+        answer: "Loki",
+        tip: "Irmão do Thor nos filmes da Marvel",
+      },
+      {
+        question: "Qual é o nome do lobo gigante que é destinado a causar o Ragnarök ?",
+        options: ["Jormungandr", "Hel", "Sleipnir", "Fenrir"],
+        answer: "Fenrir",
+        tip: "É um lobo gigante",
+      },
+      {
+        question: "Quem são os seres que governam os destinos dos deuses e dos homens na mitologia nórdica?",
+        options: ["As valquírias", "Os anões", "Os jotuns", "As Nornas"],
+        answer: "As Nornas",
+        tip: "São três anciãs da mitologia nórdica que moram em uma das raízes de Yggdrasil",
+      },
+      {
+        question: "Qual é o nome do deus da guerra na mitologia nórdica?",
+        options: [
+          "Thor",
+          "Freyja",
+          "Balder",
+          "Tyr"],
+        answer: "Tyr",
+        tip: "Deus nórdico associado à guerra, coragem e justiça.",
+      },
+      {
+        question: "Qual o nome da serpente gigante que envolve o mundo dos humanos até a chegada de Ragnarok.",
+        options: ["Fenrir", "Jörmundgander", "Sleipnir", "Garmr"],
+        answer: "Jörmundgander",
+        tip: "Também é conhecido por muitos como Serpente de Midgard",
+      },
+    ],
+  },
+  {
+    category: "Egípcia",
+    questions: [
+      {
+        question: "Qual é o deus do sol na mitologia egípcia?",
+        options: [
+          "Osíris",
+          "Anúbis",
+          "Hórus",
+          "Rá",
+        ],
+        answer: "Rá",
+        tip:"É associado com um gavião.",
+      },
+      {
+        question: "Quem é o deus associado à morte, ao embalsamamento e ao julgamento dos mortos?",
+        options: ["Osíris", "Seth", "Anúbis", "Ptah"],
+        answer: "Anúbis",
+        tip:"O personagem de League Of Legends Nasus foi baseado nele.",
+      },
+      {
+        question: "O que Hórus perdeu na batalha contra Seth",
+        options: ["Sua cabeça", "Mão direita", "Suas armas", "Um olho"],
+        answer: "Um olho",
+        tip:"Possuia dois.",
+      },
+      {
+        question: "Contra o quê Anúbis pesa o coração dos mortos ?",
+        options: ["O olho de Hórus", "A pena da verdade", "O cetro de Osíris", "A alma do morto"],
+        answer: "A pena da verdade",
+        tip:"As aves possuem",
+      },
+      {
+        question:
+          "Qual o nome do Deus egípcio da sabedoria?",
+        options: ["Sobek", "Nekhbet", "Rá", "Thoth"],
+        answer: "Thoth",
+        tip:"Também é responsavel pela escrita.",
+      },
+    ],
+  },
+];
+
+export default data;
